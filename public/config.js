89114b943a8c285fc8a4fd5813866e611c1757c4
Mario Reder <mario.reder@pm.me>
2025-10-16 00:49:43 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -23,7 +23,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
   "VITE_TWITTER_URL": "",
-  "VITE_SEO_SITE_NAME": "",
+  "VITE_SEO_SITE_NAME": "Paidish",
   "VITE_SEO_SITE_DESCRIPTION": "",
   "VITE_SEO_SITE_URL": "https://dex.orderly.network/paidish-2514/",
   "VITE_SEO_SITE_LANGUAGE": "en",
